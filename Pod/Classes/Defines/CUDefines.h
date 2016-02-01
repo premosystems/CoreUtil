5f7faf86007e40cad3b3c251897e42bae4e6e4b8
Vincil Bishop <vincil.bishop@vbishop.com>
2016-02-01 15:04:03 -0600

    Updated for blocks

--- Pod/Classes/Defines/CUDefines.h
+++ Pod/Classes/Defines/CUDefines.h
@@ -9,5 +9,8 @@
 #ifndef Pods_CUDefines_h
 #define Pods_CUDefines_h
 
+typedef void (^CUBlock)(id result);
+typedef void (^CUCompletionBlock)(id sender, id result);
+typedef void (^CUErrorBlock)(id sender, BOOL success, NSError *error, id result);
 
 #endif
